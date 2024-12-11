b9a921d42c514be6a986fbb215c8d46e4873fe5d
Yahaya Azeez <azconcept2016@gmail.com>
2024-12-11 15:50:25 +0100

    add express server

---
+++ server.js
@@ -0,0 +1,18 @@
+import { express } from 'express';
+import router from './routes';
+
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || 'localhost';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', router);
+
+app.listen(port, host, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
