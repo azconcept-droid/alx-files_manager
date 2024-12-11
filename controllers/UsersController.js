144dbf5d932f03608f9dc9cc2db5dd8ec5e8b078
Yahaya Azeez <azconcept2016@gmail.com>
2024-12-11 21:28:34 +0100

    fix bug

---
+++ controllers/UsersController.js
@@ -0,0 +1,21 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+export default class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+    if (!password) return res.status(400).json({ error: 'Missing password' });
+
+    if (await dbClient.userCollection.findOne({ email })) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const user = await dbClient.userCollection.insertOne({
+      email,
+      password: sha1(password),
+    });
+
+    return res.status(201).json({ id: user.insertedId, email });
+  }
+}
